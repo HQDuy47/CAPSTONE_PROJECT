cbe4272ef5738dd1d965093c18f20ff1bf58b82d
HQDuy47 <72744215+HQDuy47@users.noreply.github.com>
2024-06-12 19:05:11 +0700

    clone project

--- frontend/src/app/my-jobs/empty.tsx
+++ frontend/src/app/my-jobs/empty.tsx
@@ -5,7 +5,7 @@ interface EmptyField {
 const EmptyField: React.FC<EmptyField> = ({ title }) => {
   return (
     <div className=' w-full rounded-xl bg-white py-10'>
-      <div className='flex flex-col items-center justify-center gap-4 '>
+      <div className='flex flex-col items-center justify-center gap-5 '>
         <img
           src='https://itviec.com/assets/everything-empty-62c813bcb84be8a092033e40550b6fdc9f6bda05947d60c619b2a74906144f8b.svg'
           alt='itviec-empty'
